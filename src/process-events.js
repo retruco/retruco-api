84bd8b692e25b31bc3b84f469f025400fd05b59a
Emmanuel Raviart <emmanuel@raviart.com>
2016-10-26 22:17:13 +0200

    Add handling of rating events for cards & properties.

--- src/process-events.js
+++ src/process-events.js
@@ -19,8 +19,24 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
+import assert from "assert"
+import deepEqual from "deep-equal"
+
 import {checkDatabase, db, entryToBallot, entryToEvent, entryToStatement} from "./database"
 import {addBallotEvent} from "./model"
+
+
+function addRatedValue(requestedSchemaType, values, schema, value) {
+  assert(requestedSchemaType !== "array")
+  let schemaType = schema.type
+  if (schemaType === "array") {
+    for (let itemValue of value) {
+      addRatedValue(requestedSchemaType, values, schema.items.type, itemValue)
+    }
+  } else if (schemaType === requestedSchemaType) {
+    values.add(value)
+  }
+}
 
 
 async function describe(statement) {
@@ -48,8 +64,12 @@ async function describe(statement) {
     ))
     const groundDescription = await describe(ground)
     return `argument for ${claimDescription} based on ${groundDescription}` 
+  } else if (type === "Card") {
+    return `card ${statement.id}`
   } else if (type === "PlainStatement") {
     return `plain statement ${statement.languageCode}"${statement.name}"`
+  } else if (type === "Property") {
+    return `property ${statement.name} = ${statement.value}`
   } else if (type === "Tag") {
     const taggedStatement = entryToStatement(await db.oneOrNone(
         `SELECT * FROM statements
@@ -65,42 +85,37 @@ async function describe(statement) {
 
 
 async function processEvent(event) {
-  await db.none(`DELETE FROM events WHERE id = $<id>`, event)
+  await db.none("DELETE FROM events WHERE id = $<id>", event)
   if (event.type === "rating") {
     let statement = entryToStatement(await db.oneOrNone(
-      `SELECT * FROM statements WHERE id = $<statementId>`,
+      "SELECT * FROM statements WHERE id = $<statementId>",
       event,
     ))
     if (statement === null) return
     let description = await describe(statement)
-    console.log(`Processing event ${event.type} of ${event.createdAt.toISOString()} for ${description}...`)
+    console.log(`Processing ${event.type} of ${event.createdAt.toISOString()} for ${description}...`)
 
     // Compute statement rating.
     let ratingCount = 0
     let ratingSum = 0
-    console.log('a')
     let ballots = (await db.any(
-      `SELECT * FROM ballots WHERE statement_id = $<id>`,
+      "SELECT * FROM ballots WHERE statement_id = $<id>",
       statement,
     )).map(entryToBallot)
-    console.log('b')
     for (let ballot of ballots) {
       ratingCount += 1
       if (ballot.rating) ratingSum += ballot.rating
     }
     let groundArguments = (await db.any(
-      `SELECT * FROM statements WHERE (data->>'claimId')::bigint = $<id>`,
+      "SELECT * FROM statements WHERE (data->>'claimId')::bigint = $<id>",
       statement,
     )).map(entryToStatement)
-    console.log('c')
     for (let argument of groundArguments) {
       if (!argument.isAbuse && (argument.rating || 0) > 0 && ["because", "but"].includes(argument.argumentType)) {
-        console.log('d')
         let ground = entryToStatement(await db.oneOrNone(
-          `SELECT * FROM statements WHERE id = $<groundId>`,
+          "SELECT * FROM statements WHERE id = $<groundId>",
           argument,
         ))
-        console.log('e')
         if (!ground.isAbuse && ground.ratingCount) {
           ratingCount += ground.ratingCount
           ratingSum += (argument.argumentType === "because" ? 1 : -1) * ground.ratingSum
@@ -108,7 +123,6 @@ async function processEvent(event) {
       }
     }
 
-    console.log('f')
     if (ratingCount != statement.ratingCount || ratingSum != statement.ratingSum) {
       // Save statement rating.
       let rating
@@ -118,14 +132,12 @@ async function processEvent(event) {
           ratingCount: 0,
           ratingSum: 0,
         })
-        console.log('g')
         await db.none(
           `UPDATE statements
             SET rating = DEFAULT, rating_count = DEFAULT, rating_sum = DEFAULT
             WHERE id = $<id>`,
           statement,
         )
-        console.log('h')
       } else {
         rating = ratingSum / ratingCount
         Object.assign(statement, {
@@ -133,17 +145,15 @@ async function processEvent(event) {
           ratingCount,
           ratingSum,
         })
-        console.log('i')
         await db.none(
           `UPDATE statements
             SET rating = $<rating>, rating_count = $<ratingCount>, rating_sum = $<ratingSum>
             WHERE id = $<id>`,
           statement,
         )
-        console.log('j')
       }
 
-      // Propagate stateemnt rating change.
+      // Propagate statement rating change.
       let claimArguments = (await db.any(
         `SELECT * FROM statements
           WHERE (data->>'groundId')::bigint = $<id>`,
@@ -185,6 +195,84 @@ async function processEvent(event) {
         }
       } else if (statement.type === "Argument") {
         await addBallotEvent(statement.claimId)
+      } else if (statement.type === "Property") {
+        let cardEntry = await db.oneOrNone(
+            `SELECT id, data FROM statements
+              WHERE id = $<statementId>`,
+            statement,
+        )
+        if (cardEntry !== null) {
+          let sameNameProperties = (await db.any(
+            `SELECT * FROM statements
+              WHERE type = 'Property'
+              AND (data->>'statementId')::bigint = $<cardId>
+              AND data->>'name' = $<propertyName>
+              ORDER BY rating DESC, id DESC`,
+            {
+              cardId: cardEntry.id,
+              propertyName: statement.name,
+            },
+          )).map(entryToStatement)
+          let cardData = cardEntry.data
+          let oldCardData = JSON.parse(JSON.stringify(cardData))
+          let removeAttribute = true
+          if (sameNameProperties.length > 0) {
+            // TODO: Improve search of best property. For example, if any of the best rated properties is of type
+            // "rated set", it wins even when it is not the oldest (lowest id).
+            let bestProperty = sameNameProperties[0]
+            let bestRating = bestProperty.rating
+            if (bestRating > 0) {
+              // Sometimes the best property is not the oldest of the best rated properties.
+              for (let property of sameNameProperties) {
+                if (property.rating < bestRating) break
+                if (property.widget.tag === "RatedSet") {
+                  let requestedSchemaType = property.schema.type
+                  let ratedValues = new Set()
+                  for (let property1 of sameNameProperties) {
+                    if (property1.rating <= 0) break
+                    addRatedValue(requestedSchemaType, ratedValues, property1.schema, property1.value)
+                  }
+                  bestProperty = {
+                    schema: property.schema,
+                    value: [...ratedValues].sort(),
+                    widget: property.widget,
+                  }
+                  break
+                }
+              }
+
+              removeAttribute = false
+              if (!cardData.values) cardData.values = {}
+              cardData.values[bestProperty.name] = bestProperty.value
+              if (!cardData.schemas) cardData.schemas = {}
+              cardData.schemas[bestProperty.name] = bestProperty.schema
+              if (!cardData.widgets) cardData.widgets = {}
+              cardData.widgets[bestProperty.name] = bestProperty.widget
+            }
+          }
+          if (removeAttribute) {
+            if (cardData.values) {
+              delete cardData.values[statement.name]
+              if (cardData.values.length === 0) delete cardData.values
+            }
+            if (cardData.schemas) {
+              delete cardData.schemas[statement.name]
+              if (cardData.schemas.length === 0) delete cardData.schemas
+            }
+            if (cardData.widgets) {
+              delete cardData.widgets[statement.name]
+              if (cardData.widgets.length === 0) delete cardData.widgets
+            }
+          }
+          if (!deepEqual(cardData, oldCardData)) {
+            await db.none(
+              `UPDATE statements
+                SET data = $<data>
+                WHERE id = $<id>`,
+              cardEntry,
+            )
+          }
+        }
       } else if (statement.type === "Tag") {
         let taggedEntry = await db.oneOrNone(
             `SELECT data FROM statements
