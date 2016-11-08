2da7c1af9d703d402e48a3bda735bec31ad64da0
Emmanuel Raviart <emmanuel@raviart.com>
2016-11-08 22:35:45 +0100

    Decrease server timeout to 5 minutes.

--- src/server.js
+++ src/server.js
@@ -177,5 +177,5 @@ function startExpress() {
   let server = app.listen(port, host, () => {
     console.log(`Listening on ${host || "*"}:${port}...`)
   })
-  server.timeout = 30 * 60 * 1000  // 30 minutes (in milliseconds)
+  server.timeout = 5 * 1000  // 5 minutes (in milliseconds)
 }
