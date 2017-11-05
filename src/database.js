d71531b9c386bfaca47c14e001cc680e53fe1e54
Emmanuel Raviart <emmanuel@raviart.com>
2017-11-05 21:51:27 +0100

    Increase database version number.

--- src/database.js
+++ src/database.js
@@ -34,7 +34,7 @@ export const db = pgPromise({
 })
 export let dbSharedConnectionObject = null
 
-export const versionNumber = 30
+export const versionNumber = 31
 export const versionTextSearchNumber = 5
 
 export { checkDatabase }
